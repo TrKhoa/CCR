ef271860fbce54b629955e6142858753ebf74867
TrKhoa <khoatmtfx14250@funix.edu.vn>
2023-03-19 16:04:46 +0700

    vercel test 5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5050;
+app.get('/', (req, res) => { res.send('<html>NODE</html>')});
 app.listen(PORT, ()=> console.log('running on port ' + PORT));
